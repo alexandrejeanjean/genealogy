6cc381ac133b67dae324070767dec4cb0cbea073
Alexandre Jeanjean <alexandre@dotmind.io>
2020-03-06 15:38:35 +0100

    [Auth] Created a simple authentication with JWT

--- server/controllers/peoples.js
+++ server/controllers/peoples.js
@@ -1,46 +1,61 @@
 const People = require("../models").People;
+const getToken = require("../helpers").getToken;
 
 module.exports = {
   create(req, res) {
-    return People.create({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      familyId: req.params.familyId,
-      generationId: req.params.generationId
-    })
-      .then(people => res.status(201).send(people))
-      .catch(error => res.status(400).send(error));
+    var token = getToken(req.headers);
+    if (token) {
+      return People.create({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        familyId: req.params.familyId,
+        generationId: req.params.generationId
+      })
+        .then(people => res.status(201).send(people))
+        .catch(error => res.status(400).send(error));
+    } else {
+      return res.status(403).send({ success: false, msg: "Unauthorized." });
+    }
   },
 
   list(req, res) {
-    console.log("REQ", req.body);
-    return People.findAll({
-      where: {
-        familyId: req.params.familyId,
-        generationId: req.params.generationId
-      }
-    })
-      .then(peoples => res.status(200).send(peoples))
-      .catch(error => res.status(400).send(error));
+    var token = getToken(req.headers);
+    if (token) {
+      return People.findAll({
+        where: {
+          familyId: req.params.familyId,
+          generationId: req.params.generationId
+        }
+      })
+        .then(peoples => res.status(200).send(peoples))
+        .catch(error => res.status(400).send(error));
+    } else {
+      return res.status(403).send({ success: false, msg: "Unauthorized." });
+    }
   },
 
   destroy(req, res) {
-    return People.findOne({
-      where: {
-        familyId: req.params.familyId,
-        generationId: req.params.generationId,
-        id: req.params.peopleId
-      }
-    })
-      .then(people => {
-        if (!people) {
-          return res.status(404).send({ message: "People not found" });
+    var token = getToken(req.headers);
+    if (token) {
+      return People.findOne({
+        where: {
+          familyId: req.params.familyId,
+          generationId: req.params.generationId,
+          id: req.params.peopleId
         }
-        return people
-          .destroy()
-          .then(() => res.status(204).send())
-          .catch(error => res.status(400).send(error));
       })
-      .catch(error => res.status(400).send(error));
+        .then(people => {
+          if (!people) {
+            return res.status(404).send({ message: "People not found" });
+          }
+          return people
+            .destroy()
+            .then(() => res.status(204).send())
+            .catch(error => res.status(400).send(error));
+        })
+        .catch(error => res.status(400).send(error));
+    } else {
+      return res.status(403).send({ success: false, msg: "Unauthorized." });
+    }
   }
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,9 @@
 const familiesController = require("../controllers").families;
 const generationsController = require("../controllers").generations;
 const peoplesController = require("../controllers").peoples;
+const usersController = require("../controllers").users;
+const passport = require("passport");
+
 module.exports = app => {
   app.get("/api", (req, res) =>
     res.status(200).send({
@@ -8,30 +11,58 @@ module.exports = app => {
     })
   );
 
+  // User routes
+  app.post("/api/signup", usersController.create);
+  app.post("/api/signin", usersController.signin);
+
   // Families routes
-  app.post("/api/families", familiesController.create);
-  app.get("/api/families", familiesController.list);
-  app.delete("/api/families/:familyId", familiesController.destroy);
+  app.post(
+    "/api/families",
+    passport.authenticate("jwt", { session: false }),
+    familiesController.create
+  );
+  app.get(
+    "/api/families",
+    passport.authenticate("jwt", { session: false }),
+    familiesController.list
+  );
+  app.delete(
+    "/api/families/:familyId",
+    passport.authenticate("jwt", { session: false }),
+    familiesController.destroy
+  );
 
   // Generations routes
-  app.post("/api/families/:familyId/generations", generationsController.create);
-  app.get("/api/families/:familyId/generations", generationsController.list);
+  app.post(
+    "/api/families/:familyId/generations",
+    passport.authenticate("jwt", { session: false }),
+    generationsController.create
+  );
+  app.get(
+    "/api/families/:familyId/generations",
+    passport.authenticate("jwt", { session: false }),
+    generationsController.list
+  );
   app.delete(
     "/api/families/:familyId/generations/:generationId",
+    passport.authenticate("jwt", { session: false }),
     generationsController.destroy
   );
 
   // Peoples routes
   app.post(
     "/api/families/:familyId/generations/:generationId/peoples",
+    passport.authenticate("jwt", { session: false }),
     peoplesController.create
   );
   app.get(
     "/api/families/:familyId/generations/:generationId/peoples",
+    passport.authenticate("jwt", { session: false }),
     peoplesController.list
   );
   app.delete(
     "/api/families/:familyId/generations/:generationId/peoples/:peopleId",
+    passport.authenticate("jwt", { session: false }),
     peoplesController.destroy
   );
 };
