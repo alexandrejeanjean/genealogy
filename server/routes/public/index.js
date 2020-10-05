d3a6a59f932fdb0b6ed212bc28634b8a65df1892
Home <home@localhost.localdomain>
2020-10-05 16:34:55 +0200

    [Middleware] Added middleware on signup to check if username already exist

--- server/routes/public/index.js
+++ server/routes/public/index.js
@@ -1,13 +1,18 @@
 const usersController = require("../../controllers").users;
+const userMiddlewares = require("../../middlewares").user;
 
-module.exports = app => {
+module.exports = (app) => {
   app.get("/api", (req, res) =>
     res.status(200).send({
-      message: "Welcome to the genealogy API!"
+      message: "Welcome to the genealogy API!",
     })
   );
 
   // User routes
-  app.post("/api/signup", usersController.create);
+  app.post(
+    "/api/signup",
+    userMiddlewares.checkUsername,
+    usersController.create
+  );
   app.post("/api/signin", usersController.signin);
 };
